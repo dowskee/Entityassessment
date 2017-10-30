35c333c01989b3e1119f1ae3e082a3fb31dcfb13
dowskee <31831031+dowskee@users.noreply.github.com>
2017-10-30 16:05:19 -0400

    new code

---
+++ EntityFrameworkAssessment/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EntityFrameworkAssessment.Models;
+
+namespace EntityFrameworkAssessment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            NorthwindEntities NorthwindDB = new NorthwindEntities();
+
+            List<Order_Detail> OrderIdentities = NorthwindDB.Order_Details.ToList();
+            List<Customer> CustomerIdentities = NorthwindDB.Customers.ToList();
+            List<Employee> EmployeeIdentities = NorthwindDB.Employees.ToList();
+
+            ViewBag.OrdersID = OrderIdentities;
+            ViewBag.CustomersTable = CustomerIdentities;
+            ViewBag.EmployeesTable = EmployeeIdentities;
+
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult OrdersTable(int OrderID, string CustomerID, int EmployeeID)
+        {
+            NorthwindEntities NorthwindDB = new NorthwindEntities();
+
+            return View("About");
+        }
+    }
+}
